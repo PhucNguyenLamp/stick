e5eb7904a08697f77ef93fadd4f145069ac74426
Gia Phuc <giaphuc29082004@gmail.com>
2024-10-06 22:19:53 +0700

    Add smoother dragging, prevent out-of-bound

--- frontend/src/components/Note.js
+++ frontend/src/components/Note.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Note.css'
 
 export default function Note({header, message, position, onDrag, onMouseDown, isActive}) {
     const [isDragging, setIsDragging] = React.useState(false);
     const [dragOffset, setDragOffset] = React.useState({ x: 0, y: 0 });
-
     const handleMouseDown = e => {
         setIsDragging(true);
         setDragOffset({
             x: e.clientX - position.x,
             y: e.clientY - position.y
-        })
+        });
         onMouseDown();
     }
     const handleMouseMove = e => {
-        if (isDragging) {
-            onDrag(e.clientX - dragOffset.x, e.clientY - dragOffset.y);
-        }
+        const windowWidth = window.innerWidth;
+        const windowHeight = window.innerHeight;
+        const newWidth = Math.max(0, Math.min(windowWidth, e.clientX - dragOffset.x));
+        const newHeight = Math.max(0, Math.min(windowHeight, e.clientY - dragOffset.y));
+        onDrag(newWidth, newHeight);
     }
     const handleMouseUp = e => {
         setIsDragging(false);
     }
+    useEffect(() => {
+        if (isDragging) {
+            window.addEventListener('mousemove', handleMouseMove);
+            window.addEventListener('mouseup', handleMouseUp);
+        } else {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseup', handleMouseUp);
+        }
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseup', handleMouseUp);
+        }
+    }, [isDragging]);
   return (
       <div className={`note ${isActive ? 'active' : ''}`} 
         style={{ left: position.x, top: position.y}}
         onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
       >
         <strong>{header}</strong>
         <p>{message}</p>
