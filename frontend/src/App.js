9825467ea0acc9fca3cbece39eeb7ce0aebf78cf
Gia Phuc <giaphuc29082004@gmail.com>
2024-10-07 15:35:32 +0700

    update add note button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,14 @@ function App() {
   const [notes, setNotes] = React.useState([{ header: "helloWorld", message: "Helloworld", position: { x: 50, y: 60 } }]);
   const [activeNote, setActiveNote] = React.useState(null);
   
-  const fetchNotes = async () => {
-    const response = await fetch('http://localhost:8000/notes');
-    const data = await response.json();
-    setNotes(data);
-  }
-  useEffect(() => {
+  // const fetchNotes = async () => {
+  //   const response = await fetch('http://localhost:8000/notes');
+  //   const data = await response.json();
+  //   setNotes(data);
+  // }
+  // useEffect(() => {
 
-  }, [])
+  // }, [])
   const addNote = () => {
     const newNote = {
       header: "newNote",
@@ -35,7 +35,9 @@ function App() {
   }
   return (
     <div >
-      <button onClick={addNote}>+Add note+</button>
+      <div onClick={addNote}>
+        <svg class="add" viewBox="0 0 448 512" height={32} width={32}><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z" /></svg>
+      </div>
       {
         notes.map((note, index) => {
           return <Note key={index} header={note.header} message={note.message} position={note.position} onDrag={(x, y) => updateNotePosition(index, x, y)}
